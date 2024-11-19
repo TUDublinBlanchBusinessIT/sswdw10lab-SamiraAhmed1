86392b29bb4924ff05ba9fcc17f3adaeed1e1712
Samira Ahmed <B00156786@mytudublin.ie>
2024-11-19 14:43:08 +0000

    part 1 done

---
+++ processForm1.php
@@ -0,0 +1,24 @@
+<?php
+session_start(); 
+
+
+    
+    $_SESSION['passengerFN'] = $_POST['firstname'];
+    $_SESSION['passengerSN'] = $_POST['surname'];
+    if (isset($_POST['luggage'])) {
+        $_SESSION['luggage'] = 0;
+        header('Location: luggage.html');
+    }
+    else{
+        $_SESSION['luggage'] = 0;
+        header('Location: finalstep.html');
+
+
+
+    }
+
+
+
+    
+   
+    ?>
